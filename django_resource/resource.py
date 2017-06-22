4cb2a1a9c50fe59ddef62c7900ec88ed8ba7122e
Luiz Menezes <luiz.menezesf@gmail.com>
2017-06-22 12:55:39 -0300

    Fix Resource.Meta endpoints handling on ResourceBase

--- django_resource/resource.py
+++ django_resource/resource.py
@@ -11,18 +11,17 @@ class ResourceBase(type):
 
         meta = attrs.pop('Meta', None)
         if meta is None:
-            raise ValueError(
-                "Resource {} must specify a Meta class defining at least "
-                "the resource's main endpoints.".format(new_class.__name__)
-            )
+            raise ValueError("Resource {} must specify a Meta class.".format(new_class.__name__))
 
-        if not meta.endpoints:
+        try:
+            endpoints = meta.endpoints
+        except AttributeError:
             raise ValueError(
-                "Resource {} must specify a Meta class with attribute 'endpoints'.".format(new_class.__name__)
+                "Resource {} Meta class must have an 'endpoints' attribute.".format(new_class.__name__)
             )
 
         client_class = getattr(meta, 'client_class', ResourceClient)
-        meta.client = client_class(endpoints=meta.endpoints)
+        meta.client = client_class(endpoints=endpoints)
 
         new_class._meta = meta
         new_class.objects = ResourceManager(resource_class=new_class)
--- tests/test_resource.py
+++ tests/test_resource.py
@@ -1,6 +1,25 @@
-from django_resource.resource import ResourceManager
+import pytest
+
+from django_resource.resource import Resource, ResourceManager
 
 
 def test_resource_construct(gist_resource):
     assert gist_resource.objects
     assert isinstance(gist_resource.objects, ResourceManager)
+
+
+def test_resource_construct_without_meta(gist_resource):
+    with pytest.raises(ValueError) as exc:
+        class InvalidResource(Resource):
+            pass
+
+    assert 'must specify a Meta class' in str(exc)
+
+
+def test_resource_construct_invalid_meta(gist_resource):
+    with pytest.raises(ValueError) as exc:
+        class InvalidResource(Resource):
+            class Meta:
+                pass
+
+    assert 'Meta class must have' in str(exc)
